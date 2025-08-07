b63bf8dcc86a59107c183d6a9905af0791f152b2
DavidGinovyan25 <david48952@gmail.com>
2025-08-07 19:58:03 +0300

    fix: correct error print in parse

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include "bmp_data.h"
-
-//Y - HEIGHT; X - WIGHT; 
+ 
 int main(int argc, char *argv[]) {
+    if (parse(argc, argv) == false) {
+        std::cout << "ERROR: uknown command" << std::endl;
+        exit(1);
+    }
     ExtractExtremePoints();
     CalculateImageSize();
     uint64_t **bmp_pixel_data;
     PrepearFrame(bmp_pixel_data);
     SetSandAtCoordinates(bmp_pixel_data);
     SetBmpPixelColor(bmp_pixel_data);
-    //WriteFullToBmp(bmp_pixel_data);
+    WriteFullToBmp(bmp_pixel_data);
 }
 
